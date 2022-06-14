d5510c50cf7b9d8a001bf05d76f84640f863a3fb
Jonathan <jamorochop@uce.edu.ec>
2022-06-13 21:25:14 -0500

    JM: Paso de parámetros

--- holaMundo/src/app/articulo/articulo.component.ts
+++ holaMundo/src/app/articulo/articulo.component.ts
@@ -1,10 +1,7 @@
+import { JsonPipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-
-interface Articulo {
-  nombre: string,
-  descripcion: string,
-  precio: number
-}
+import { Router } from '@angular/router';
+import { Articulo } from '../models/articulo';
 
 @Component({
   selector: 'app-articulo',
@@ -15,26 +12,36 @@ export class ArticuloComponent implements OnInit {
 
   articulos: Array<Articulo> = new Array<Articulo>();
 
-  constructor() { }
+  constructor(private ruta: Router) { }
 
   ngOnInit(): void {
     this.articulos.push(
       {
         nombre: 'Televisor de 24 pulgadas',
         descripcion: 'Marca Dell',
-        precio: 1500
+        precio: 1500,
+        stock: 10,
+        precioMayorista: 1300
       },
       {
         nombre: 'Laptop i5',
         descripcion: 'Laptop marca Dell',
-        precio: 2000
+        precio: 2000,
+        stock: 20,
+        precioMayorista: 1000
       },
       {
         nombre: 'Monitor',
         descripcion: 'Monitor curvo 4k',
-        precio: 400
+        precio: 400,
+        stock: 10,
+        precioMayorista: 365
       }
     )
+  }
+
+  pasarParametro(articuloRecibido:Articulo){
+    this.ruta.navigate(['articuloDetalle',{articulo: JSON.stringify(articuloRecibido)}]);
   }
 
 }
