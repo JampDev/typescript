4cb00fb89cdfe21ee7839dbe4698599eb91245b0
Jonathan <jamorochop@uce.edu.ec>
2022-08-23 21:17:19 -0500

    JM: Guardar cliente

--- MasterGym/src/app/agregar-cliente/agregar-cliente.component.ts
+++ MasterGym/src/app/agregar-cliente/agregar-cliente.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
@@ -11,8 +12,10 @@ export class AgregarClienteComponent implements OnInit {
 
   datosFormulario: FormGroup;
   porcentajeCarga: number = 0;
+  urlImagen: string = "";
 
-  constructor(private fb: FormBuilder, private storage: AngularFireStorage) { }
+  constructor(private fb: FormBuilder, private storage: AngularFireStorage,
+    private db: AngularFirestore) { }
 
   ngOnInit(): void {
     this.datosFormulario = this.fb.group({
@@ -29,26 +32,31 @@ export class AgregarClienteComponent implements OnInit {
   }
 
   agregar() {
+    this.datosFormulario.value.imgUrl = this.urlImagen;
+    this.datosFormulario.value.fechaNacimiento = new Date(this.datosFormulario.value.fechaNacimiento);
     console.log(this.datosFormulario.value);
+    this.db.collection('clientes').add(this.datosFormulario.value).then((trm) => {
+      console.log('Registro Creado');
+    });
   }
 
   subirImagen(evento: any) {
-    let archivo = evento.target.files[0];
-    let nombre = new Date().getTime().toString();
-    let extension = archivo.name.toString().substring(archivo.name.toString().lastIndexOf('.'));
-    let ruta = 'clientes/' + nombre + extension;
-    const referencia = this.storage.ref(ruta);
-    const tarea = referencia.put(archivo);
-    tarea.then(obj => {
-      console.log('Imagen subida');
-      referencia.getDownloadURL().subscribe(url => {
-        console.log(url);
+    if (evento.target.files.length > 0) {
+      let archivo = evento.target.files[0];
+      let nombre = new Date().getTime().toString();
+      let extension = archivo.name.toString().substring(archivo.name.toString().lastIndexOf('.'));
+      let ruta = 'clientes/' + nombre + extension;
+      const referencia = this.storage.ref(ruta);
+      const tarea = referencia.put(archivo);
+      tarea.then(obj => {
+        console.log('Imagen subida');
+        referencia.getDownloadURL().subscribe(url => {
+          this.urlImagen = url;
+        });
       });
-    });
-    tarea.percentageChanges().subscribe(porcentaje => {
-      this.porcentajeCarga = parseInt(porcentaje.toString());
-    });
-
-
+      tarea.percentageChanges().subscribe(porcentaje => {
+        this.porcentajeCarga = parseInt(porcentaje.toString());
+      });
+    }
   }
 }
